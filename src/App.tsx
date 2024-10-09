c8cb3b97e315fa64fb22e7998fbacbfe541e4d88
Yuliana Chavez <ychavez@Yulianas-MacBook-Pro.local>
2024-10-09 12:03:36 -0700

    react part of lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+import { dummyNotesList } from "./constant"; // Import the dummyNotesList from the appropriate module
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div className='app-container'>
+     {/* inserted here */}
+     <form className="note-form">
+       <div><input placeholder="Note Title"></input></div>
+
+       <div><textarea></textarea></div>
+
+       <div><button type="submit">Create Note</button></div>
+</form>
+
+<div className="notes-grid">
+       {dummyNotesList.map((note) => (
+         <div
+           key={note.id}
+           className="note-item">
+           <div className="notes-header">
+             <button>x</button>
+           </div>
+           <h2> {note.title} </h2>
+           <p> {note.content} </p>
+           <p> {note.label} </p>
+         </div>
+       ))}
+     </div>
+
+       <button>
+         x
+       </button>
+       <h2> 1st Note Title </h2>
+       <p> 1st Note Content </p>
+       <p> 1st Note Label </p>
+     </div>
+
+ );
 }
 
 export default App;
